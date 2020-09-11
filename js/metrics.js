f55a3a76de0cf5694edb362e8d6b925c91af16db
Luuk van der Meer <luuk.vandermeer@adyen.com>
2020-09-11 15:14:19 +0200

    Removed shitshow

--- js/metrics.js
+++ js/metrics.js
@@ -5,7 +5,7 @@ d3.json('data.json').then(function(data) {
   var workoutsCountWeek = workoutsCount / 52; //devide to count week per year
   var workoutsHours = d3.sum(data, function(d) {return d.elapsed_time / 3600}) //devide to translate to hours
   var workoutsTogether = d3.count(data, d => {if (d.athlete_count >= 2) {return d.id}})
-  var workoutsHoursAloneMean = d3.mean(data, function(d) {if (d.athlete_count >= 1) {return d.elapsed_time / 3600}}) //devide to translate to hours
+  var workoutsHoursAloneMean = d3.mean(data, function(d) {if (d.athlete_count == 1) {return d.elapsed_time / 3600}}) //devide to translate to hours
   var workoutsHoursTogetherMean = d3.mean(data, function(d) {if (d.athlete_count >= 2) {return d.elapsed_time / 3600}}) //devide to translate to hours
   var workoutsAloneTogether;if (((workoutsHoursTogetherMean - workoutsHoursAloneMean) / workoutsHoursAloneMean) >= 0) {workoutsAloneTogether = "longer";} else {workoutsAloneTogether = "shorter"} //if statement for shorter or longer rides together
   var countriesVisitedCount = d3.nest() //create seperate array to count the values. Calculation in the span
@@ -15,12 +15,15 @@ d3.json('data.json').then(function(data) {
   var tuffestDay = d3.max(data, function(d) {if (d.suffer_score >= (d3.max(data, d => d.suffer_score))) {return d.start_date_local}}) //determine tuffestscore and get start_date_local
   var tuffestDayDetails = d3.max(data, function(d) {if (d.suffer_score >= (d3.max(data, d => d.suffer_score))) {return [(d3.format(",.1f")((d.distance / 1000))) + "km", d.total_elevation_gain + "m", (d3.format(",.1f")(d.moving_time / 3600)) + "h", d.suffer_score + "pt"]}}) //determine tuffestscore and get details
   var farthestDistance = d3.max(data, function(d) {if (d.distance >= (d3.max(data, d => d.distance))) {return (d.distance)}})
-  var farthestDistanceDetails = d3.max(data, function(d) {if (d.distance >= (d3.max(data, d => d.distance))) {return [(d3.timeFormat("%B %d")(d3.timeParse("%Y-%m-%dT%H:%M:%SZ")(d.start_date_local))), (d3.format(",.1f")(d.moving_time / 3600)) + "h"]}}) //determine farthestDistance and get details
+  var farthestDistanceDetails = d3.max(data, function(d) {if (d.distance == farthestDistance) {return [(d3.timeFormat("%B %d")(d3.timeParse("%Y-%m-%dT%H:%M:%SZ")(d.start_date_local))), (d3.format(",.1f")(d.moving_time / 3600)) + "h"]}}) //determine farthestDistance and get details
+  var shortestDistance = d3.min(data, function(d) {if (d.distance || Infinity){return d.distance}}) //Infintity excludes zero values
+  var shortestDistanceDetails = d3.min(data, function(d) {if (d.distance == shortestDistance){return [(d3.timeFormat("%B %d")(d3.timeParse("%Y-%m-%dT%H:%M:%SZ")(d.start_date_local))), (d3.format(",.1f")(d.moving_time / 3600)) + "h"]}}) //determine shortestDistance and get details
+  var workoutsVirtual = d3.count(data, d=> {if (d.type == "VirtualRide") {return d.id}})
 
+  var workoutsHoursRealDealMean = d3.mean(data, function(d) {if (d.type || "VirtualRide") {return d.elapsed_time / 3600}}) //devide to translate to hours
+  var workoutsHoursVirtualMean = d3.mean(data, function(d) {if (d.type == "VirtualRide") {return d.elapsed_time / 3600}}) //devide to translate to hours
+  var workoutsRealdealVirtual;if (((workoutsHoursVirtualMean - workoutsHoursRealDealMean) / workoutsHoursRealDealMean) >= 0) {workoutsRealdealVirtual = "longer";} else {workoutsRealdealVirtual = "shorter"} //if statement for shorter or longer rides Realdeal vs Virtual
 
-  var shortestDistance = d3.min(data, function(d) {if (d.distance > 0.1){return d.distance}})
-  // var shortestDistanceDetails = d3.min(data, function (d) {if (d.distance >= (d3.min(data,  d => (d.distance>0.1))) {return [(d3.timeFormat("%B %d")(d3.timeParse("%Y-%m-%dT%H:%M:%SZ")(d.start_date_local))), (d3.format(",.1f")(d.moving_time / 3600)) + "h"]}}) //determine farthestDistance and get details
-console.log(shortestDistance);
 
 
   var climbing = d3.sum(data, function(d) {return d.total_elevation_gain})
@@ -45,6 +48,11 @@ console.log(shortestDistance);
   d3.select(".farthestDistanceDetails").text((farthestDistanceDetails.join(' | ')))
   d3.select(".shortestDistance").text((d3.format(",.0f")((shortestDistance))))
   d3.select(".shortestDistanceDetails").text((shortestDistanceDetails.join(' | ')))
+
+  d3.select(".workoutsRealdeal").text((workoutsCount - workoutsVirtual))
+  d3.select(".workoutsVirtual").text(workoutsVirtual)
+  d3.select(".workoutsHoursChangeVirtual").text((d3.format("+.0%")((workoutsHoursVirtualMean - workoutsHoursRealDealMean) / workoutsHoursRealDealMean)) + " " + (workoutsRealdealVirtual))
+
   d3.select(".climbing").text(d3.format(",.0f")(climbing) + "m")
   d3.select(".climbingDetails").text(d3.format(".1f")(climbingDetails) + "x")
   d3.select(".calories").text(d3.format(",.0f")(calories))
